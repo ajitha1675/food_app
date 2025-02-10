b79c03feb10326a3e1a7dca3104413faad6ba5f3
Ajilalitha <ajilalitha010@gmail.com>
2025-02-10 19:00:34 +0530

    previous searches section

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,7 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
 export default function Recipes(){
+  const searches = [ 'Pizza', 'Burgar', 'Cookies', 'Juice', 'Briyani', 'Salad', 'Ice cream', 'Lasagna', 'Pudding', 'Soup'
+  ]
+
   return (
     <div>
-      Recipes Page
+        <div className="previous-searches section">
+            <h2>Previous Searches</h2>
+            <div className="previous-searches-container">
+              {searches.map((search, index) => (<div key = {index} style={{animationDelay: index * .1 + "s"}} className="search-items">
+                 {search}
+              </div>))}
+              <div className="search-box">
+                <input type="text" placeholder="Search..."/>
+                <button className="btn"><FontAwesomeIcon icon={faSearch}/></button>
+                
+              </div>
+            </div>
+        </div>
     </div>
   )
 }
