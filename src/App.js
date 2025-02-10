20d59a42b3f907203b444d8405242b98efeabf4e
Ajilalitha <ajilalitha010@gmail.com>
2025-02-10 12:49:22 +0530

    creates the hero section1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Navbar from "./components/Navbar";
-
+import Herosection from "./components/Herosection";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className="container main">
+        <Herosection />
+      </div>
     </div>
   );
 }
