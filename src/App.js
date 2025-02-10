ac6d8b23f1e48d29bb2fadb194ae1f6e3bc083c8
Ajilalitha <ajilalitha010@gmail.com>
2025-02-10 15:10:02 +0530

    footer section added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Herosection from "./components/Herosection";
 import ImproveSkill from "./components/ImproveSkill";
 import QuoteSection from "./components/QuoteSection";
 import ChefSection from "./components/ChefSection";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <ImproveSkill />
         <QuoteSection />
         <ChefSection />
+        <Footer />
       </div>
     </div>
   );
